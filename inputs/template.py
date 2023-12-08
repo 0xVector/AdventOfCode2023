a9ed5337b3542a765f1dc740d884dff9ca39af05
0xVector <hello@0xvector.me>
2023-12-08 11:15:06 +0100

    template

---
+++ inputs/template.py
@@ -0,0 +1,13 @@
+with open("inputs/day6.txt") as file:
+    data = [line.strip() for line in file]
+
+# Part 1 ===
+part1 = 0
+
+
+# Part 2 ===
+part2 = 0
+
+
+print("Part 1:", part1)
+print("Part 2:", part2)
